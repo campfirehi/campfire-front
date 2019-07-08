e5116fd3695dcfe5b16c967413fcead1610d3828
Man Chon Kuok <manchonkuok@gmail.com>
2019-07-07 18:26:51 -1000

    make members to public

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -18,8 +18,8 @@ class TeamMember {
 })
 export class AboutUsComponent implements OnInit {
 
-  private members: TeamMember[] = [
-    new TeamMember('Man chon Kuok', 'The Go-To Person', 'I am good', '/assets/img/people/mckuok.jpg'),
+  members: TeamMember[] = [
+    new TeamMember('Man chon Kuok', 'The Go-To Person', 'I am good', 'assets/img/people/mckuok.jpg'),
     new TeamMember('Romin Valdez', 'The Cooportate Person', 'I am better', ''),
     new TeamMember('Jason Leong', 'The "I can do that" Person', 'I amd da best', '')
   ];
