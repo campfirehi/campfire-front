241316bdd4af14bcc26a0145c40f9ed023176f55
jleong8 <jleong8@hawaii.edu>
2019-07-10 20:48:40 -1000

    Completed login and registration

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -9,11 +9,11 @@ function googleLogin() {
     firebase.auth().signInWithPopup(provider)
 
         .then(result => {
-            const user = result.user;
-            document.write(`Hello ${user.displayName}`);
-            console.log(user);
+            displayStatus(true, 'Welcome ' + result.user.displayName, 4000);
         })
-        .catch(console.log);
+        .catch(function(error) {
+            displayStatus(false, error.message, 8000);
+        });
 }
 
 function register(){
@@ -21,31 +21,56 @@ function register(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(firebaseUser) {
+        displayStatus(true, 'Welcome to the site ' + firebaseUser.user.email, 4000);
+    })
+    .catch(function(error) {
+        displayStatus(false, error.message, 8000);
     });
-  
-  }
+}
 
 function login(){
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(firebaseUser) {
+        console.log(firebaseUser);
+        displayStatus(true, 'Welcome ' + firebaseUser.user.email, 4000);
+    })
+    .catch(function(error) {
+        displayStatus(false, error.message, 8000);
     });
-  
   }
+
+ function displayStatus(saveMessage, message, timeInMilliseconds)
+ {
+    $("#status").removeClass();
+     var errorMessage = $("#status");
+     if (saveMessage)
+     {
+         errorMessage.show();
+         var myInterval = window.setInterval(function()
+         {
+             errorMessage.text(message);
+             errorMessage.show();
+             $("#status").addClass("alert-success");
+             $
+         }, 1000);
+         window.setTimeout(function()
+         {
+             clearInterval(myInterval);
+             errorMessage.hide();
+             $("#status").removeClass("alert-success");
+         }, timeInMilliseconds);
+     }
+     else
+     {
+         errorMessage.text(message);
+         errorMessage.show();
+         $("#status").addClass("alert-danger");
+         window.setTimeout('$("#status").hide()', timeInMilliseconds);
+     };
+ };
