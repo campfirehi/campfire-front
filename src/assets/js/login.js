0d40bb94fc123b544b26c07243cafdf2e1edf2a4
jleong8 <jleong8@hawaii.edu>
2019-07-10 20:17:59 -1000

    Added login and register

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -15,3 +15,37 @@ function googleLogin() {
         })
         .catch(console.log);
 }
+
+function register(){
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+
+function login(){
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
