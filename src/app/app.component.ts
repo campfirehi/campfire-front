4e05c38d2ef221b3b236cc657f0ca11b4c40727e
man chon <manchonkuok@gmail.com>
2019-08-16 21:44:10 -1000

    dynamic resizing of navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'campfire';
+  height = 100;
+
+  @ViewChild('navbar', { read: ElementRef, static: false }) navbar;
+  @ViewChild('footer', { read: ElementRef, static: false }) footer;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const maxHeight = this.vh2Px(100);
+      console.log(this.navbar.nativeElement.offsetHeight)
+      this.height = maxHeight - this.navbar.nativeElement.offsetHeight - this.footer.nativeElement.offsetHeight;
+      console.log()
+
+    }, 0)
+  }
+
+  vh2Px(vh) {
+    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return vh * h / 100;
+  }
 }
